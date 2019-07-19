ac1803e3940b3d58f46e314418bdcd112fc5496f
zq <2903780002@qq.com>
2019-07-19 21:24:44 +0800

    gaigaigai

--- OrderSystem/src/org/test/Test.java
+++ OrderSystem/src/org/test/Test.java
@@ -4,6 +4,10 @@ import org.order.view.LoginView;
 
 public class Test  {
 	public static void main(String[] args){		
-		new LoginView("登陆").init();		
+		new LoginView("登陆").init();	
+		
+		
+		
+		System.out.println("hehehehheheehehh");
 	}
 }
